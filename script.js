4be9b30917b016db0b8a4d8541f329ff6332e270
Mateus Vianna <panzerkovic@gmail.com>
2024-02-08 14:43:21 -0300

    aula 7

---
+++ script.js
@@ -0,0 +1,7 @@
+const pokemon = { name: 'Pikachu', growl :"Pika Pika" };
+const saySomething = `${pokemon.growl}`;
+const goPokemon = `Go ${pokemon.name}!`;
+
+// console.log(saySomething); // Pika Pika
+// console.log(goPokemon); // Go Pikachu!
+
