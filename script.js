18f64cc9f6c054b05384fa3496936e514000009f
Mateus Vianna <panzerkovic@gmail.com>
2024-02-19 08:48:53 -0300

    Not sure about this

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const pokemon = { name: 'Pikachu', growl :"Pika Pika" };
-const saySomething = `${pokemon.growl}`;
-const goPokemon = `Go ${pokemon.name}!`;
+const fetchPromise = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+);
 
-// console.log(saySomething); // Pika Pika
-// console.log(goPokemon); // Go Pikachu!
+console.log(fetchPromise);
 
+fetchPromise.then((resposta) => {
+  return resposta.json();
+});
+
+const init = async () => {
+  const [a, b] = Promise.all([fetchPromise, fetchPromise]);
+  const response = await fetchPromise;
+  console.log(response);
+};
+init();
+
+console.log("Requisição iniciada…");
