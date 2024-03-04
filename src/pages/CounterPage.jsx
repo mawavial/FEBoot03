0cfca484ae495fd69e2efc743785f53c1bffa39e
mawavial <panzerkovic@gmail.com>
2024-03-04 16:38:22 -0300

    strange and babeling

--- src/pages/CounterPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-
-const CounterPage = () => {
-    return (<h1>Use Reducer</h1>)
-}
-
-export default CounterPage
--- src/utils/homeRouteConstants.js
+++
@@ -1,4 +0,0 @@
-
-export const REGISTER_PAGE = 'register'
-export const WELCOME_PAGE = 'welcome'
-export const COUNTER_PAGE = 'counter'
