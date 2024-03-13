a6689ae78f757bbb2fc792049f2e0f44d76215d7
Mateus Vianna <panzerkovic@gmail.com>
2024-03-13 18:04:19 -0300

    persiste reducer

--- redux-js/src/app/store.js
+++ redux-js/src/app/store.js
@@ -1,8 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../features/counter/counterSlice'
+import storage from 'redux-persist/lib/storage'
+import sessionStorage from 'redux-persist/lib/storage/session'
+import { persistReducer, persistStore } from 'redux-persist'
+import { thunk } from 'redux-thunk'
+
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// }
+
+const persistConfig = {
+  key: 'root',
+  sessionStorage,
+}
+
+//state.value
+//persistor.state.counter.value
+const persistedReducer = persistReducer(persistConfig, counterReducer)
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+  reducer: persistedReducer,
+  middleware: [ thunk ]
+})
+
+export const persistor = persistStore(store)
